81763b6dc47b70539c26be99f59b70fb66b7a7e2
Siddhesh Kankekar <kankekas@tcd.ie>
2021-05-04 08:04:16 +0100

    Deployment

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBf-F3CWrUAnXI0tFlwmnzJwzGhKwA6sOc",
+  authDomain: "project-hulu.firebaseapp.com",
+  projectId: "project-hulu",
+  storageBucket: "project-hulu.appspot.com",
+  messagingSenderId: "124625130482",
+  appId: "1:124625130482:web:10887cecb77e926b81d16b",
+  measurementId: "G-764KNSG5E4",
+};
